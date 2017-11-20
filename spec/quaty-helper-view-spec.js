745740b5f3db5ece181ae78177e93f094974e360
CN40606-N <CN40606-N@CN40606-N.local>
2017-11-20 13:48:17 +0800

    add files

---
+++ spec/quaty-helper-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import QuatyHelperView from '../lib/quaty-helper-view';
+
+describe('QuatyHelperView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
